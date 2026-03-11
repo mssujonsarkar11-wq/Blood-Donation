603708c990eddb6e6a83e218db3ddbd6d3dd1634
mssujonsarkar11-wq <mssujonsarkar11@gmail.com>
2026-03-11 15:04:51 +0600

    demo.js

---
+++ demo.js
@@ -0,0 +1,19 @@
+const donors=[
+
+{name:"Rahim",blood:"A+",district:"Gazipur",phone:"01700000001",donations:12},
+
+{name:"Karim",blood:"B+",district:"Gazipur",phone:"01700000002",donations:9},
+
+{name:"Sadia",blood:"O+",district:"Sylhet",phone:"01700000003",donations:15},
+
+{name:"Nabila",blood:"AB+",district:"Sylhet",phone:"01700000004",donations:7}
+
+]
+
+const requests=[
+
+{patient:"Hasan",blood:"A+",hospital:"DMCH",contact:"01711111111",urgency:"Emergency"},
+
+{patient:"Rafi",blood:"O+",hospital:"Square Hospital",contact:"01722222222",urgency:"Normal"}
+
+]
